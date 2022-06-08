468c349f2187113353b38c2079781c710ff024a0
erfanhamD <erfan.hamdi@gmail.com>
2022-06-08 15:35:30 +0430

    changed the name of function

--- Logistic/main.py
+++ Logistic/main.py
@@ -1,163 +1,84 @@
-from typing import Callable
-
-import matplotlib.pyplot as plt
-import torch
-from scipy.integrate import solve_ivp
 from torch import nn
-import numpy as np
+import torch
+import matplotlib.pyplot as plt
 
-R = 1.0
-F0 = 1.0
-
-
-class NNApproximator(nn.Module):
-    def __init__(self, num_hidden: int, dim_hidden: int, act=nn.Tanh()):
-
-        super().__init__()
-
-        self.layer_in = nn.Linear(1, dim_hidden)
-        self.layer_out = nn.Linear(dim_hidden, 1)
-
-        num_middle = num_hidden - 1
-        self.middle_layers = nn.ModuleList(
-            [nn.Linear(dim_hidden, dim_hidden) for _ in range(num_middle)]
+class Logistic_PINN(nn.Module):
+    def __init__(self):
+        super(Logistic_PINN, self).__init__()
+        self.layer_in = nn.Linear(1, 10)
+        self.layer_out = nn.Linear(10, 1)
+        self.hidden_layers = nn.ModuleList(
+            [nn.Linear(10, 10) for _ in range(9)]
         )
-        self.act = act
+        self.act = nn.Tanh()
+    
+    def forward(self, t):
+        out_0 = self.layer_in(t)
+        for layer in self.hidden_layers:
+            out_0 = self.act(layer(out_0))
+        out_1 = self.layer_out(out_0)
+        return out_1
 
-    def forward(self, x):
-        out = self.act(self.layer_in(x))
-        for layer in self.middle_layers:
-            out = self.act(layer(out))
-        return self.layer_out(out)
+def f(nn, t_train):
+    return nn(t_train)
 
-
-def f(nn: NNApproximator, x: torch.Tensor) -> torch.Tensor:
-    """Compute the value of the approximate solution from the NN model"""
-    return nn(x)
-
-
-def df(nn: NNApproximator, x: torch.Tensor = None, order: int = 1) -> torch.Tensor:
-    """Compute neural network derivative with respect to input features using PyTorch autograd engine"""
-    df_value = f(nn, x)
-    for _ in range(order):
-        df_value = torch.autograd.grad(
-            df_value,
-            x,
-            grad_outputs=torch.ones_like(x),
-            create_graph=True,
-            retain_graph=True,
+def df(nn, t_train, order=1):
+    df_eval = nn(t_train)
+    for d in range(order):
+        df_eval = torch.autograd.grad(
+            df_eval, t_train, grad_outputs=torch.ones_like(t_train), create_graph=True, retain_graph=True
         )[0]
+    return df_eval
 
-    return df_value
-
-
-def compute_loss(
-    nn: NNApproximator, x: torch.Tensor = None, verbose: bool = False
-) -> torch.float:
-    """Compute the full loss function as interior loss + boundary loss
-
-    This custom loss function is fully defined with differentiable tensors therefore
-    the .backward() method can be applied to it
-    """
-
-    interior_loss = df(nn, x) - R * x * (1 - x)
-
-    boundary = torch.Tensor([0.0])
+def loss_func(nn_instance, t_train):
+    interior_loss = df(nn_instance, t_train) - R * t_train*(1-t_train)
+    boundary = torch.Tensor([T0])
     boundary.requires_grad = True
-    boundary_loss = f(nn, boundary) - F0
-    final_loss = interior_loss.pow(2).mean() + boundary_loss ** 2
-    return final_loss
+    boundary_loss = f(nn_instance, boundary) - F0
+    total_loss = interior_loss.pow(2).mean() + boundary_loss**2
+    return total_loss
 
-
-def train_model(
-    nn: NNApproximator,
-    loss_fn: Callable,
-    learning_rate: int = 0.01,
-    max_epochs: int = 1_000,
-) -> NNApproximator:
-
-    loss_evolution = []
-
-    optimizer = torch.optim.SGD(nn.parameters(), lr=learning_rate)
-    for epoch in range(max_epochs):
-
-        try:
-
-            loss: torch.Tensor = loss_fn(nn)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            if epoch % 1000 == 0:
-                print(f"Epoch: {epoch} - Loss: {float(loss):>7f}")
-
-            loss_evolution.append(loss.detach().numpy())
-
-        except KeyboardInterrupt:
-            break
-
-    return nn, np.array(loss_evolution)
-
-
-def check_gradient(nn: NNApproximator, x: torch.Tensor = None) -> bool:
-
-    eps = 1e-4
-    dfdx_fd = (f(nn, x + eps) - f(nn, x - eps)) / (2 * eps)
-    dfdx_sample = df(nn, x, order=1)
-
-    return torch.allclose(dfdx_fd.T, dfdx_sample.T, atol=1e-2, rtol=1e-2)
-
+def train_func(nn_instance, t_train, epochs=20_000, lr=0.01):
+    optimizer = torch.optim.Adam(nn_instance.parameters(), lr=lr)
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+        loss = loss_func(nn_instance, t_train)
+        loss_list = []
+        loss_list.append(loss.item())
+        loss.backward()
+        optimizer.step()
+        if epoch % 10 == 0:
+            print("Epoch: {}, Loss: {}".format(epoch, loss.item()))
+    return nn_instance, loss_list
 
 if __name__ == "__main__":
-    from functools import partial
+    
+    # Colocation Points
+    t_loss = torch.linspace(0, 1, steps = 100, requires_grad=True)
+    t_loss = t_loss.reshape(t_loss.shape[0], 1)
 
-    domain = [0.0, 1.0]
-    x = torch.linspace(domain[0], domain[1], steps=10, requires_grad=True)
-    x = x.reshape(x.shape[0], 1)
+    t_train = torch.linspace(0, 1, steps = 10, requires_grad=True)
+    t_train = t_train.reshape(t_train.shape[0], 1)
 
-    nn_approximator = NNApproximator(4, 10)
-    assert check_gradient(nn_approximator, x)
+    # Boundary and Initial Conditions
+    T0 = 0
+    F0 = 1
 
-    # f_initial = f(nn_approximator, x)
-    # ax.plot(x.detach().numpy(), f_initial.detach().numpy(), label="Initial NN solution")
+    # Funciton Parameters
+    R = 1
 
-    # train the PINN
-    loss_fn = partial(compute_loss, x=x, verbose=True)
-    nn_approximator_trained, loss_evolution = train_model(
-        nn_approximator, loss_fn=loss_fn, learning_rate=0.1, max_epochs=20_000
-    )
+    # NN Model
+    nn_instance = Logistic_PINN()
 
-    x_eval = torch.linspace(domain[0], domain[1], steps=100).reshape(-1, 1)
-
-    # numeric solution
-    def logistic_eq_fn(x, y):
-        return R * x * (1 - x)
-
-    numeric_solution = solve_ivp(
-        logistic_eq_fn, domain, [F0], t_eval=x_eval.squeeze().detach().numpy()
-    )
-
-    # plotting
+    #Train the Model
+    nn_trained, loss_list = train_func(nn_instance, t_train)
+    
+    # Function Evaluation
     fig, ax = plt.subplots()
 
-    f_final_training = f(nn_approximator_trained, x)
-    f_final = f(nn_approximator_trained, x_eval)
+    f_final_training = f(nn_trained, t_train)
+    f_final = f(nn_trained, t_loss)
 
-    ax.scatter(x.detach().numpy(), f_final_training.detach().numpy(), label="Training points", color="red")
-    ax.plot(x_eval.detach().numpy(), f_final.detach().numpy(), label="NN final solution")
-    ax.plot(
-        x_eval.detach().numpy(),
-        numeric_solution.y.T,
-        label=f"Analytic solution",
-        color="green",
-        alpha=0.75,
-    )
-    ax.set(title="Logistic equation solved with NNs", xlabel="t", ylabel="f(t)")
-    ax.legend()
-
-    fig, ax = plt.subplots()
-    ax.semilogy(loss_evolution)
-    ax.set(title="Loss evolution", xlabel="# epochs", ylabel="Loss")
-    ax.legend()
-
+    ax.scatter(t_train.detach().numpy(), f_final_training.detach().numpy(), label="Training points", color="red")
+    ax.plot(t_loss.detach().numpy(), f_final.detach().numpy(), label="NN final solution")
     plt.show()
